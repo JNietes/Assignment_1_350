ba910d8d133df9aa7a1514dccb97ff0eb39ffee8
Kent Palabon <palabok@sunypoly.edu>
2024-09-23 13:27:09 -0400

    Create kent.sql

---
+++ kent.sql
@@ -0,0 +1,12 @@
+/* Problem 2 */
+/* Author: Kent Palabon */
+
+/* Problem 3 */
+/* Author: Kent Palabon */
+
+/* Problem 5 */
+/* Author: Kent Palabon */
+
+/* Problem 6 */
+/* Author: Kent Palabon */
+
