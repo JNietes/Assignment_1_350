164839b2104ff1d3353438490e148067cb4b9614
Joaquin Nietes <nietesj@sunypoly.edu>
2024-09-25 13:24:28 -0400

    Completed problems 7, 10, and 13

--- joaquin.sql
+++ joaquin.sql
@@ -15,10 +15,30 @@ and e2.last_name = 'Greenberg'
 
 /* Problem 7 */
 /* Author: Joaquin Nietes */
+select department_id, count(*), avg(salary)
+from employees
+group by department_id having count(*) > 8
+order by department_id asc
 
 
 /* Problem 10 */
 /* Author: Joaquin Nietes */
+select count(*) from employees
+inner join jobs
+on employees.department_id = departments.department_id
+where department_name = 'IT' and salary > 
+
+(select max(salary) from employees inner join jobs
+on employees.job_id = jobs.job_id 
+inner join departments
+on employees.department_id = departments.department_id
+where department_name = 'Shipping' and job_title like '%Manager%')
+
 
 /* Problem 13 */
 /* Author: Joaquin Nietes */
+select region_name from regions
+where region_name like 'Z%'
+intersect
+select region_name from regions
+where region_id = '4'
